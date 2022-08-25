f5840fa65ce1fab87bb6c14b9146a54ee00e5c97
KingoftheSandwings <benjamin.e.seitz@gmail.com>
2022-08-25 19:31:17 +0000

    added minor clarification

--- hello.c
+++ hello.c
@@ -9,6 +9,7 @@ int main (int argc, char **argv)
 {
     printf("Hello, this is Benjamin Seitz!\n");
     printf("My major is Electrical Engineering :)\n");
+    printf("I do not have a minor.\n");
 
     return 0;
 }
