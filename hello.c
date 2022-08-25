8b974efcfaac068ceed506b610c85cee04f547df
Kenner Jensen <kjensen25@huskers.unl.edu>
2022-08-25 20:04:36 +0000

    Added my name & major

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@ int main (int argc, char **argv)
     printf("Hello, this is Benjamin Seitz!\n");
     printf("My major is Electrical Engineering :)\n");
     printf("I do not have a minor.\n");
-    printf("And my name is Kenner Jensen, and I'm majoring in Computer Science.\n");
+    printf("And my name is Kenner Jensen, and I'm majoring in Computer Science (the best major).\n");
 
     return 0;
 }
