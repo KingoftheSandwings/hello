95f84407ffe36ed9ae4a2fb722467527788a9c71
KingoftheSandwings <benjamin.e.seitz@gmail.com>
2022-08-25 19:05:33 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello
+ */
+
+int main (int argc, char **argv)
+{
+    printf("Hello, this is Benjamin Seitz!\n");
+    printf("My major is Electrical Engineering :)\n");
+
+    return 0;
+}
